7a3988605fd9956368036fc8f913f9ca1f318094
mihsastv <30728392+mihsastv@users.noreply.github.com>
2020-02-02 13:15:10 +0300

    FinalCommit1

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,7 +1,7 @@
 import {url} from "../config/url";
 import axios from "axios";
 
-///асинхронное выполнение кода
+
 
 export  function addCar(data) {
     return {
@@ -36,8 +36,9 @@ export  function getCarsCalendar(id) {
 
         await axios.get(url+'cars/'+id)
             .then(function (response) {
-                const data = response.data
+                const data = {data: response.data, id: id};
                 dispatch(addCarCalendar(data))
+                dispatch(setMonth({month: new Date().getMonth(), year: new Date().getFullYear()}))
             })
             .catch(function (error) {
                 console.log(error);
@@ -52,22 +53,48 @@ export  function deleteBooking(id) {
     }
 }
 
-export  function setMonth(id) {
-    return {
-        type: 'SET_MONTH',
-        payload: id
-    }
-}
-
-export  function deletedBooking(id) {
+export  function deletedBooking(data) {
     return async (dispatch) => {
-        await axios.delete(url+'cars/unbooking/'+id )
+        await axios.delete(url+'cars/unbooking/'+data.id )
             .then(function (response) {
-                const data = response.data
-                if (data._id === id) dispatch(deleteBooking(id))
+                const rez = response.data
+                if (rez._id === data.id) {dispatch(deleteBooking(data.id));
+                dispatch(setMonth({month: data.months, year: data.year}))}
             })
             .catch(function (error) {
                 console.log(error);
             });
+    }
+}
+
+export  function postBooking(data) {
+    return {
+        type: 'ADD_BOOKING',
+        payload: data
+    }
+}
+
+export  function addBooking(period) {
+    return async (dispatch) => {
+        const data = {
+                dateFrom: period.dateFrom,
+                dateTo: period.dateTo
+        }
+        await axios.post(url+'cars/booking/'+period.currentCar, data)
+            .then(function (response) {
+                const rez = response.data
+                dispatch(postBooking(rez))
+                dispatch(setMonth({month: new Date(period.dateFrom).getMonth(), year: new Date(period.dateFrom).getFullYear()}))
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+}
+
+export  function setMonth(data) {
+    return {
+        type: 'SET_MONTH',
+        payload: data
     }
 }
